d970a792d084d9b563735c07133df4b38572e732
Blake Geraci <bgeraci851@hcc.edu>
2019-03-26 18:19:04 -0400

    fixes name of CanvasContainer

--- src/js/components/presentational/styled/CanvasContainer.js
+++ src/js/components/presentational/styled/CanvasContainer.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const ButtonGroup = styled.div`
+const CanvasContainer = styled.div`
   display: flex;
 `;
 
-export default ButtonGroup;
+export default CanvasContainer;
