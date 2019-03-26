b2fed2305a06dfa2ca517e24a6cdefadae6ab6f9
Daniil Maly <dan@1x-nat-vl930-172-30-97-47.wireless.umass.edu>
2019-03-26 16:45:52 -0400

    Fixed the changes, as requested

--- src/js/components/container/ButtonContainer.js
+++ src/js/components/container/ButtonContainer.js
@@ -9,38 +9,40 @@ class ButtonContainer extends Component {
     this.state = {
       buttonPressed: 0
     };
-    this.click = this.click.bind(this);
+    this.onClickHandler = this.onClickHandler.bind(this);
   }
-  click(event){
+
+  onClickHandler(event){
       this.setState({ [event.target.id]: event.target.value });
   }
+
   render() {
     const { buttonPressed } = this.state;
     return ( 
         <div class = "btn-group">
-            <ButtonInput
-            label="buttonPressed"
-            text="Clear"
-            type= {ButtonStyle}
-            id="buttonPressed"
-            value="Clear"
-            click={this.click}
+              <ButtonInput
+                label="buttonPressedClear"
+                text="Clear"
+                type= {ButtonStyle}
+                id="ClearButton"
+                value="Clear"
+                onClickHandler={this.onClickHandler}
             />
-            <ButtonInput
-            label="buttonPressed"
-            text="Color"
-            type={ButtonStyle}
-            id="buttonPressed"
-            value="Color"
-            click={this.click}
+              <ButtonInput
+                label="buttonPressedColor"
+                text="Color"
+                type={ButtonStyle}
+                id="ColorButton"
+                value="Color"
+                onClickHandler={this.onClickHandler}
             />
-            <ButtonInput
-            label="buttonPressed"
-            text="Size"
-            type={ButtonStyle}
-            id="buttonPressed"
-            value="Size"
-            click={this.click}
+              <ButtonInput
+                label="buttonPressedSize"
+                text="Size"
+                type={ButtonStyle}
+                id="SizeButton"
+                value="Size"
+                onClickHandler={this.onClickHandler}
             />
         </div> 
     );
--- src/js/components/presentational/ButtonStyle.js
+++ src/js/components/presentational/ButtonStyle.js
@@ -2,16 +2,11 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyledButton = styled.button`
-width: ${({width}) => (width ? `${width}px` : '50px')};
-height: ${({ height}) => (height ? `${height}px` : '25px')};
-color: ${({color}) => (color ? color:'blue')};
-cursor: ${({cursor}) => (cursor ? cursor: 'pointer')};
-name: ${({name}) => (name ? name:'Buttone')};
-background: ${({background}) => (background ? background:'lightblue')};
-label: ${({label}) => (label ? label:'inherit')};
-value: ${({value}) => (value ? value:"inherit")};
-
+    width: ${({width}) => (width ? `${width}px` : '4rem')};
+    height: ${({ height}) => (height ? `${height}px` : '4rem')};
+    color: ${({color}) => (color ? color:'blue')};
+    cursor: ${({cursor}) => (cursor ? cursor: 'pointer')};
+    background: ${({background}) => (background ? background:'lightblue')};
 `;
-
 
 export default StyledButton;
